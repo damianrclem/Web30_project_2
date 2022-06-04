2c71c431ad188e15eedcb2ad4446e23e3ff39b4f
Damian Clem <damianrclem@gmail.com>
2022-06-03 19:30:14 -0700

    static helper data

---
+++ client/lib/static.js
@@ -0,0 +1,40 @@
+export const news = [
+    {
+        category: 'Clever Programmer • LIVE',
+        title: 'CP is live on YouTube!',
+        image: 'https://picsum.photos/200?random=2',
+    },
+    {
+        category: 'Databases • Trending',
+        title: 'Sanity is giving you free Boosted plan, use --coupon cleverprogrammer',
+        image: 'https://picsum.photos/200?random=4',
+    },
+    {
+        category: 'Web3.0 • Trending',
+        title: 'Blockchain developers make over $150k',
+        image: 'https://picsum.photos/200?random=8',
+    },
+    {
+        category: 'ReactJS • Trending',
+        title: 'Join Profit with React while spots last',
+        image: 'https://picsum.photos/200?random=12',
+    },
+];
+
+export const whoToFollow = [
+    {
+        name: 'Rafeh Qazi',
+        handle: '@cleverqazi',
+        avatar: 'https://i.pravatar.cc/300?img=1',
+    },
+    {
+        name: 'David Rakosi',
+        handle: '@0xDavidRakosi',
+        avatar: 'https://i.pravatar.cc/300?img=2',
+    },
+    {
+        name: 'Sanity.io',
+        handle: '@sanity_io',
+        avatar: 'https://i.pravatar.cc/300?img=4',
+    },
+];
