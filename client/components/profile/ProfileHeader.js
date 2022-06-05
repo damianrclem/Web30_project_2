56695500bd414674d669d2fa48d9205bc309bd90
Damian Clem <damianrclem@gmail.com>
2022-06-04 19:03:25 -0700

    profile header styling

--- client/components/profile/ProfileHeader.js
+++ client/components/profile/ProfileHeader.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import { BsArrowLeftShort } from 'react-icons/bs';
 
+import logo from '../../images/logo.svg';
+
 const cover = 'https://picsum.photos/1200/500?random=8';
+const currentAccount = '0x787fEe51E380ff7b1d76819F4682d0C21B78D6D8';
 
 const style = {
     wrapper: `border-[#38444d] border-b`,
@@ -24,6 +27,8 @@ const style = {
 const ProfileHeader = () => {
     const router = useRouter();
 
+    const isProfileImageNft = false;
+
     return (
         <div className={style.wrapper}>
             <div className={style.header}>
@@ -35,8 +40,35 @@ const ProfileHeader = () => {
                     <div className={style.secondary}>4 Tweets</div>
                 </div>
             </div>
-            <div>
+            <div className={style.coverPhotoContainer}>
                 <img src={cover} alt='cover' />
+            </div>
+            <div className={style.profileImageContainer}>
+                <div className={isProfileImageNft ? 'hex' : style.profileImage}>
+                    <img
+                        src='https://i.pravatar.cc/300?img=1'
+                        alt='profile image'
+                        className={isProfileImageNft ? style.profileImageNft : style.profileImage}
+                    />
+                </div>
+            </div>
+            <div className={style.details}>
+                <div>
+                    <div className={style.primary}>Damian Clem</div>
+                    <div className={style.secondary}>
+                        {currentAccount && (
+                            <>
+                                {currentAccount.slice(0, 5)}...{currentAccount.slice(-5)}
+                            </>
+                        )}
+                    </div>
+                </div>
+                <div className={style.nav}>
+                    <div className={style.activeNav}>Tweets</div>
+                    <div>Tweets & replies</div>
+                    <div>Media</div>
+                    <div>Likes</div>
+                </div>
             </div>
         </div>
     );
